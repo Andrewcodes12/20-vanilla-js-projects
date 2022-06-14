973713fff63c1ae4ce42d2c67a016facbded86fe
Andrewcodes12 <andrewfava@yahoo.com>
2022-06-14 12:02:45 -0400

    monday

--- Intereview prep/JS Concepts/inheritance and the prototype chain.js
+++ Intereview prep/JS Concepts/inheritance and the prototype chain.js
@@ -1,80 +1,38 @@
-// There are only two limitations:
+/*
+__proto__ is outdated
 
-// The references can’t go in circles. JavaScript will throw an error if we try to assign __proto__ in a circle.
-// The value of __proto__ can be either an object or null. Other types are ignored.
-
-// [[Prototype]] propert is internal and hidden
-
-// let animal = {
-//     eats: true
-// };
-
-// let rabbit = {
-//     jumps: true
-// }
-
-// rabbit.__proto__ = animal
+modern JS suggest we use Object.getPrototypeOf / Object.setPrototypeOf
 
 
-// if we need a property from rabbit and its missing; JS will automatically take it from animal
+let user = {
+    name: "John",
+    lastname: "smith"
 
+    set fullName(value){
+        [this.name, this,lastname] = value.split(" ")
+    },
 
-// let animal = {
-//     eats: true
-// };
-
-// let rabbit = {
-//     jumps: true
-// };
-
-
-// rabbit.__proto__ = animal
-
-// alert( rabbit.eats) // true
-// alert(rabbit.jumps)
-
-
-// animal is the prototype of rabbit
-// rabbit prototypically inherits from animal
-// animals properties and methods become inherited by rabbit
-
-
-// let animal = {
-//     eats: true,
-//     walk(){
-//         alert("Animal Walk")
-//     }
-// };
-
-// let rabbit = {
-//     jumps: true,
-//     __proto__: animal
-// }
-
-// // walk is taken from the prototype
-// rabbit.walk() // animal walk
-
-
-let animal = {
-    eats:true,
-    walk(){
-        alert("Animal Walk")
+    get fullName(){
+        return `${this.name} ${this.lastname}`
     }
 }
 
-let rabbit = {
-    jumps: true,
-    __proto__:animal
-};
-
-let longEar = {
-    earLength: 10,
-    __proto__: rabbit
+let admin = {
+    __proto__: user,
+    isAdmin: true
 }
 
+alert(adming.fullName) // "john smith"
 
-longEar.walk() // Animal Walk
-alert(longEar.jumps) // true(from rabbit)
+// setter triggers
+admin.fullName = " Andrew fava"
+
+alert(admin.fullName) "Andrew Fava, state of admin is modified"
+alert(user.fullName) "John Smith, state of user is protected"
 
 
-// if we need something from longEar and its missing javascript will look for it in rabbit and then in animal
+
+
+
+
+*/
