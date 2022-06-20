e1ccfa15b3349f0d4242a1e70964313df03b7bcf
Andrewcodes12 <andrewfava@yahoo.com>
2022-06-20 13:52:40 -0400

    commit

---
+++ HTML & CSS/Frontend vs backend.js
@@ -0,0 +1,20 @@
+/*
+browser sends request to server
+server sends response to browser(a response contains all the files the site is made of)
+
+
+
+
+front-end development
+the code that is displayed in the browser
+
+static website
+website where files are simply sent from the server to the browser as they are(no transformations)
+no database or backend set up
+
+dynamic website
+must use backend language to communicate to the database
+backend set up with database
+
+
+*/
