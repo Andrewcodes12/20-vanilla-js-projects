1c8028cc79fd05dd507418fecfef1e98bcb397de
Andrewcodes12 <andrewfava@yahoo.com>
2022-06-20 16:41:37 -0400

    commit

--- HTML/intro to html.js
+++ HTML/intro to html.js
@@ -1,6 +1,16 @@
 /*
 HTML = HyperText Markup Language
 
+HTMl consists of elements that describe different types of content:
+paragraphs,links,headings
+
+
+<p>HTML is a markup language </p> --- HTML element
+
+html elements are made of 3 parts:
+opening tag
+content of element; can be another element(child element)
+closing tag
 
 
 
