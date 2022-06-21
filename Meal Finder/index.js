c8c7b1d273a22d6b2c3fd33a62463dee49efb56d
Andrewcodes12 <andrewfava@yahoo.com>
2022-06-21 13:19:28 -0400

    m

--- Meal Finder/index.js
+++ Meal Finder/index.js
@@ -49,8 +49,63 @@ function searchMeal(e){
 
 
 
+// fetch meal by ID
+function getMealById(mealId){
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
+    .then(res => res.json())
+    .then(data => {
+        // data returns the single meal by its ID
+        const meal = data.meals[0]
 
+        addMealToDOM(meal)
+    })
+}
 
+function addMealToDOM(meal){
+    const ingredients = []
+
+    for(let i = 1; i <= 20; i++){
+        if(meal[`strIngredient${i}`]){
+            ingredients.push(`${meal[`strIngredient${i}`]} -- ${meal[`strMeasure${i}`]}`)
+        } else {
+            break;
+        }
+    }
+
+    single_mealEl.innerHTML = `
+    <div class='single-meal'>
+    <h1>${meal.strMeal}</h1>
+    <img src='${meal.strMealThumb}' alt="${meal.strMeal}" />
+    <div class="single-meal-info>
+    ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ''}
+    ${meal.strArea ? `<p>${meal.strArea}</p>` : ''}
+    </div>
+    <div class='main'>
+        <p>${meal.strInstructions}</p>
+        <h2>Ingredients</h2>
+        <ul>
+            ${ingredients.map(ingredient =>
+                `<li>${ingredient} </li>`
+            ).join("")}
+        </ul>
+    </div>
+    </div>
+    `;
+}
 
 //Event Listeneres
 submit.addEventListener('submit', searchMeal)
+mealsEl.addEventListener('click', e => {
+    const mealInfo = e.path.find(item => {
+        if(item.classList){
+            return item.classList.contains('meal-info')
+        } else {
+            return false
+        }
+    })
+
+    if(mealInfo){
+        const mealID = mealInfo.getAttribute('data-mealid')
+        getMealById(mealID)
+    }
+})
