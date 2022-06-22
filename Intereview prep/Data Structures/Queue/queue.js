ce54cb26f1200af5c571df61f28e08f387892c98
Andrewcodes12 <andrewfava@yahoo.com>
2022-06-22 14:04:44 -0400

    a

---
+++ Intereview prep/Data Structures/Queue/queue.js
@@ -0,0 +1,25 @@
+/*
+queue is a linear collection of elements that can be modified by enqueing and dequeing.
+
+elements are added to the back and removed from the front.
+
+queus can be implemented using arrays or singly linked lists.
+
+
+Time Complexity
+enqueue - O(n)
+dequeue - O(1)
+
+enqueu using an array is O(n) because you need to shift every element
+enqueue using a linkedList is O(1)
+
+
+
+
+
+
+
+
+
+
+*/
