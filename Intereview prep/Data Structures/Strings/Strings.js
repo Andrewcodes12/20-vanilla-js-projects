c6e547ba54e6e230e55235f9aea4dc871f2ff459
Andrewcodes12 <andrewfava@yahoo.com>
2022-06-22 12:31:30 -0400

    a

--- Intereview prep/Data Structures/Strings/Strings.js
+++ Intereview prep/Data Structures/Strings/Strings.js
@@ -26,10 +26,29 @@ usually the characters are limited to lower case characters.
 Techniques
 
 Counting Characters
+Count the frequency of characters in a string
+use a hashmap
+
+space required for a character counter is O(1)
+because the range of characters is usually a fixed constant.
+
+
+String of Unique characters
+
+
+Anagram
+a couple approaches:
+- sorting both strings - O(nlogn) time and O(logn) space
+- frequency counting using a hashmap - O(n) time O(1) space
 
 
 
-
+Palindromes
+a word of phrase that read the same forward or backwards
+a couple approaches:
+- reverse the string and it should be equal to itself
+- have 2 pointers at the start and end of the string. move pointers inward until they meet.
+At every point in time the characters at both pointers should match.
 
 
 
