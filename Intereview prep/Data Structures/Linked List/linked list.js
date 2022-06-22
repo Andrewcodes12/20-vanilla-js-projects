94e178d6ab230266b0853621cb3da03edd623087
Andrewcodes12 <andrewfava@yahoo.com>
2022-06-22 13:51:04 -0400

    a

---
+++ Intereview prep/Data Structures/Linked List/linked list.js
@@ -0,0 +1,23 @@
+/*
+like arrays a linked list is used to represent sequential data.
+
+its a linear collection of data elements whose order is not given by their physical placement
+in memory. unlike arrays where data is stored in sequential blocks of memory.
+
+instead each element contains an address of the next element.
+
+each node contains data and a reference to the next node
+
+Advantages
+
+insertion and deleton of a node are O(1)
+- elements do not need to be shifted
+
+disadvantages
+access time is linear because we need to travserse from the start of the list to find a node.
+we cannot key into the list ike we do arrays
+
+
+
+
+*/
---
+++ Intereview prep/Data Structures/matrix/matrix.js
@@ -0,0 +1,18 @@
+/*
+A matrix is a 2d array
+
+questions involving matrixes are usually related to dynamic programming or graph traversal
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
