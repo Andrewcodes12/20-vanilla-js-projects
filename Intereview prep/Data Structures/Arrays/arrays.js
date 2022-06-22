c65771625a2428fac101a3f1ee11b69b53dd85c0
Andrewcodes12 <andrewfava@yahoo.com>
2022-06-22 12:22:25 -0400

    a

--- Intereview prep/Data Structures/Arrays/arrays.js
+++ Intereview prep/Data Structures/Arrays/arrays.js
@@ -13,7 +13,25 @@ remove - O(n)
 remove from back - O(1)
 
 
+Be mindful about slicing or concatenating arrays in your code.
+Typically, slicing and concatenating arrays would take O(n) time.
+Use start and end indices to demarcate a subarray/range where possible.
+
+------------------------------------------------------------------------
+Techniques
+
+Sliding Window
+Applies to many subarray/substring problems.
+2 pointers usually move in the same direction but never overtake eachother.
+time complexity O(n)
 
 
+Two Pointers
+like sliding windows but the 2 pointers can cross eachother and can be on different arrays.
+When given 2 arrays to process, its common to have 1 pointer per array to compare both indexes
+from the 2 arrays
 
+Sorting the array
+is the array sorted or partially sorted? if it is use some form of binary search.
+This means the interviewer is looking for something faster than O(n)
 */
