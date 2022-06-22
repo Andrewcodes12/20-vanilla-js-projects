86a1685b863b3a1caef0682fa17ccadd0636760e
Andrewcodes12 <andrewfava@yahoo.com>
2022-06-22 12:39:15 -0400

    a

--- Intereview prep/Data Structures/Sorting and searching/sorting and searching.js
+++ Intereview prep/Data Structures/Sorting and searching/sorting and searching.js
@@ -1,14 +1,33 @@
 /*
+Time Complexity
+
+Algorithm	Time	Space
+Bubble sort	O(n2)	O(1)
+Insertion sort	O(n2)	O(1)
+Selection sort	O(n2)	O(1)
+Quicksort	O(nlog(n))	O(log(n))
+Mergesort	O(nlog(n))	O(n)
+Heapsort	O(nlog(n))	O(1)
+Counting sort	O(n + k)	O(k)
+Radix sort	O(nk)	O(n + k)
+Algorithm	Big-O
+Binary search	O(log(n))
 
 
 
+------------------------------------------------------------------------
+Things to look out for in interviews
+
+Make sure you know the time and space complexity of the language's default sorting algorithm!
+The time complexity is almost definitely O(nlog(n))).
+Bonus points if you can name the sort. In Python, it's Timsort.
 
 
+------------------------------------------------------------------------
+Techniques
 
-
-
-
-
+Sorted Inputs
+When given a sorted input, binary search should be one of the first things that come to your mind
 
 
 
