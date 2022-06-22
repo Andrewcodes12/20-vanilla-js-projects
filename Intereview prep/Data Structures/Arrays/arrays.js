f6fc40af05819807c00346d2d0cce1820d075676
Andrewcodes12 <andrewfava@yahoo.com>
2022-06-22 12:18:02 -0400

    a

--- Intereview prep/Data Structures/Arrays/arrays.js
+++ Intereview prep/Data Structures/Arrays/arrays.js
@@ -1,8 +1,16 @@
 /*
+Subarray - [2,3,6,1,5,4]
+[3,6,1] is a subArray
+[3,1,5] is a subsequence
 
-
-
-
+Time complexity-
+Access - 0(1) if you have the index
+Search - 0(n)
+search(sorted) - O(logn)
+insert - O(n)
+insert at end - O(1)
+remove - O(n)
+remove from back - O(1)
 
 
 
