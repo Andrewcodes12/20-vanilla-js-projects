f5a7f6169c53fc4deef0c16df4d15bdef83beaf9
Andrewcodes12 <andrewfava@yahoo.com>
2022-06-23 12:01:47 -0400

    a

---
+++ Intereview prep/Time & space/constant, linear, log math behavior.js
@@ -0,0 +1,80 @@
+/*
+
+Time complexity Order
+Constant
+Log
+Linear
+
+
+our function depends on n
+
+- constant function - simplest function we could design
+- constant functions should not rely on their input
+- constant functions the output does not depend on the input
+
+function(n) = 1
+input  output
+ n      f(n)
+ 1        1
+ 2        1
+ 3        1
+ 4        1
+
+our output is constantly 1
+
+
+LINEAR functions
+- whatever we give the function we get back the same thing
+- our output relies on our input
+- whenever we increase our input size we increase our output size
+
+function(n) = n
+input  output
+ n      f(n)
+ 1        1
+ 2        2
+ 3        3
+ 4        4
+
+function(n) = 3n
+input  output
+ n      f(n)
+ 1        3
+ 2        6
+ 3        9
+ 4        12
+
+- we increase our input by 3 so our output increases by 3
+
+
+LOGARITHMIC FUNCTIONS
+
+exponentiation
+2^5 = 2 * 2 * 2 * 2 * 2 = 32
+
+LOG
+2^5 = 32
+log2(32) = 5 ... 2 to the what power will give you 32?
+log2(16) = 4 .... 2 to the what power will give you 16?
+log2(16) = 4 ... how many times do you need to divide 16 by 2 to get 1
+
+log10(1000) = 3 ... 10 to the what power will give you 1000?
+
+
+f(n) = log(n)
+input  output
+
+ n      f(n)
+ 1        0
+ 2        1
+ 4        2
+ 8        3
+ 16       4
+ 32       5
+
+
+
+
+
+
+*/
