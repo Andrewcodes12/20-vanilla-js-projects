3dbd8f1ed62ef0e090c5baebb9db045aa4e91312
Andrewcodes12 <andrewfava@yahoo.com>
2022-06-23 10:58:50 -0400

    a

--- Intereview prep/Data Structures/Heap/Heap.js
+++ Intereview prep/Data Structures/Heap/Heap.js
@@ -11,8 +11,20 @@ In a heap, the children of a node must be less than or equal to the parent node
        /  \
       32   24
     /   \  /  \
-    30   9 20  18
-    
+   30   9 20  18
+   /\
+   2 7
+
+
+all children are less than or equal to the parent node
+
+
+
+There is no guaranteed order among "siblings" in a heap.
+
+In a max heap a node(root) will be greater than all of its children, grandchildren, and so on.
+In a min heap a node(root) will be less than all of its children,grandchildren, and so on.
+
 
 --------------------------------------------------------------------------------------
 A heap is a specialized tree-based structure which is a complete tree
