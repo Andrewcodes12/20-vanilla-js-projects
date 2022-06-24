acd1b732e2239482a52fbe7e83fd29fd32fe298e
Andrewcodes12 <andrewfava@yahoo.com>
2022-06-24 11:59:52 -0400

    a

---
+++ Intereview prep/CSS/Selectors/Selector.js
@@ -0,0 +1,33 @@
+/*
+A selector is a way to grab an element from HTML and then style it.
+
+-------------------------------------------------------------------
+The element selectr
+
+p {
+
+}
+
+you can select an entire element without any special character
+-------------------------------------------------------------------
+The class selector
+give a class attribute to html element
+
+.class{
+
+}
+
+multiple elements can have the same class.
+
+-------------------------------------------------------------------
+The ID selector
+used to select elements with a certain ID
+
+- IDs can only be used on one element at a time
+
+#someText{
+    
+}
+
+
+*/
