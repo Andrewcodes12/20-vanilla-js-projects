31641a450010558c074958a98379b94d87db45f1
Andrewcodes12 <andrewfava@yahoo.com>
2022-06-24 12:34:47 -0400

    a

---
+++ Intereview prep/CSS/type,properties,imports/typography.js
@@ -0,0 +1,70 @@
+/*
+properties that control how fonts are selected to use so that text gets rendered to the screen:
+
+font-family
+font-size
+font-style
+font-weight
+
+properties that affect how the text is drawn to the screen:
+text-align
+text-decoration
+text-transform
+
+
+
+-------------------------------------------------------------------
+Font-family
+instructs the browser to use a specific font.
+
+allows you to import fonts
+@import url('url')
+
+-------------------------------------------------------------------
+font size
+controls the font size
+
+
+Absolute Units
+absolute units are measured in inches or pixels
+
+
+1 px = 1/96th of an inch
+
+Relative Units
+em - the font size of the containing element
+rem - the font size of the root element
+
+
+-------------------------------------------------------------------
+Aligning your text
+text-align: center/right/left
+
+-------------------------------------------------------------------
+Decorating your text
+with text-decoration you can put lines above,through, and under text.
+lines can be solid,dashed,or wavy
+
+a{
+    text-decoration:underline currentcolor;
+
+}
+
+
+-------------------------------------------------------------------
+Casing your test
+sometimes you want text to be all upper,lower,or title case
+
+text{
+    text-transform: uppercase/lowercase/capitalize
+}
+-------------------------------------------------------------------
+
+Background images for elements
+
+logo{
+    background-image:url(url)
+    background-size: cover
+}
+
+*/
