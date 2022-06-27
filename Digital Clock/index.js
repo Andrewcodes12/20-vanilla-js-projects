22c73e97995854fdbfe6c3385f83f9732d0852bd
Andrewcodes12 <andrewfava@yahoo.com>
2022-06-27 12:35:38 -0400

    monday

--- Digital Clock/index.js
+++ Digital Clock/index.js
@@ -3,14 +3,15 @@ setInterval(getTime,1000)
 const hour = document.querySelector('.hour')
 const minute = document.querySelector('.minute')
 const second = document.querySelector('.second')
-
+const day = document.querySelector('.day')
 
 function getTime(){
 let date = new Date()
 
-let hours = date.getHours() / 12
+let hours = date.getHours()
 let minutes = date.getMinutes()
 let seconds = date.getSeconds()
+
 
 
 if(hours > 12) hours - 12
@@ -18,6 +19,7 @@ if(hours > 12) hours - 12
 hour.innerHTML = hours
 minute.innerHTML = minutes
 second.innerHTML = seconds
+
 }
 
 
